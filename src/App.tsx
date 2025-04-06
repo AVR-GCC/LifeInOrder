46bbacc5c6caba88fc5e344788f140e601e2079d
AVR-GCC <ogoun.d@gmail.com>
2025-04-06 23:39:12 +0300

    Move to lynx

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useState } from '@lynx-js/react'
+
+import './App.css'
+import arrow from './assets/arrow.png'
+import lynxLogo from './assets/lynx-logo.png'
+import reactLynxLogo from './assets/react-logo.png'
+
+export function App() {
+  const [alterLogo, setAlterLogo] = useState(false)
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.info('Hello, ReactLynx', counter)
+  }, [])
+
+  const onTap = useCallback(() => {
+    'background only'
+    setAlterLogo(!alterLogo)
+    setCounter(counter + 1)
+  }, [alterLogo])
+
+  return (
+    <view><script src="http://localhost:8097"></script>
+      <view className='Background' />
+      <view className='App'>
+        <view className='Banner'>
+          <view className='Logo' bindtap={onTap}>
+            {alterLogo
+              ? <image src={reactLynxLogo} className='Logo--react' />
+              : <image src={lynxLogo} className='Logo--lynx' />}
+          </view>
+          <text className='Title'>React</text>
+          <text className='Subtitle'>on Lynx</text>
+        </view>
+        <view className='Content'>
+          <image src={arrow} className='Arrow' />
+          <text className='Description'>Tap the logo {counter} have fun!</text>
+          <text className='Hint'>
+            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
+            to see updates!
+          </text>
+        </view>
+        <view style={{ flex: 1 }}></view>
+      </view>
+    </view>
+  )
+}
