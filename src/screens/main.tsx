ffd0f606a8c7d8a4bf5d7b6a588c1122ae9bdca2
AVR-GCC <ogoun.d@gmail.com>
2025-04-18 23:50:26 +0300

    Move to axios for api call

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,4 +1,5 @@
 import { useLynxGlobalEventListener, useCallback, useEffect, useState, useRef } from '@lynx-js/react'
+import axios from 'axios'
 import '../styles/Main.css'
 
 export const Main = () => {
@@ -17,19 +18,19 @@ export const Main = () => {
   //}, [])
   
   useEffect(() => {
-    fetch('http://10.0.0.8:8080/users/1/habit_colors')
-    .then(response => {
-      console.log('got response', response)
-      return response.json()
-    })
-    .then(data => {
-      console.log('got data', data);
-      if (data.length) {
-        const days = data[0].day_colors.map(({ date }) => date);
-        setDays(days);
-        setColumns(data);
+    const getData = async () => {
+      try {
+        const res = await axios.get('http://10.0.0.8:8080/users/1/habit_colors');
+        if (res.data.length) {
+          const days = res.data[0].day_colors.map(({ date }) => date);
+          setDays(days);
+          setColumns(res.data);
+        }
+      } catch (e) {
+        console.log('get failed', e);
       }
-    })
+    }
+    getData();
   }, [])
 
   const dayHeight = `${dayHeightPixels}px`;
@@ -52,7 +53,7 @@ export const Main = () => {
            <view className='DayMarker' style={{ height: dayHeight }}></view>
          ))}
         </view>
-        <view ref={checklistRef} className='Checklist'>
+        <view className='Checklist'>
           {columns.map(c => (
             <view className='Column' style={{ flex: c.weight.toString() }}>
               {c.day_colors.map(day_color => <view className='Square' style={{ background: day_color.color, height: dayHeight }} />)}
