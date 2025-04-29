ae52de24bc14483325a55b7e0a4ce48b6a94a3df
AVR-GCC <ogoun.d@gmail.com>
2025-04-29 23:30:40 +0300

    Day title

--- src/screens/day/index.tsx
+++ src/screens/day/index.tsx
@@ -1,8 +1,8 @@
 import { useLynxGlobalEventListener, useCallback, useEffect, useState, useRef } from '@lynx-js/react'
 import { useNavigate, useParams } from 'react-router';
+import moment from 'moment'
 import '../../styles/Day.css';
 import LeftArrow from '../../assets/arrow-left.png';
-import arrow from '../../assets/arrow.png';
 import type { SetDayHabitValue, GetDayHabitValue, MainProps } from '../../App.jsx';
 import HabitButton from './habitButton.jsx';
 
@@ -10,22 +10,23 @@ export const Day = (props: { data: MainProps, getDayHabitValue: GetDayHabitValue
   const { date } = useParams();
   if (props.data === null || date === undefined) return <text>Loading...</text>;
   const dateIndex = parseInt(date, 10);
-  const { data: { habits }, setDayHabitValue, getDayHabitValue } = props;
+  const { data: { dates, habits }, setDayHabitValue, getDayHabitValue } = props;
   const nav = useNavigate();
 
   return (
     <view>
       <view className='Buffer' />
-      <view style={{ display: 'flex' }}>
-      <view
-        className='BackArrowContainer'
-        bindtap={() => nav('/')}
-      >
-        <image
-          className='BackArrow'
-          src={LeftArrow}
-        />
-      </view>
+      <text className='DayTitle'>{moment(dates[dateIndex].date).format('MMM, DD YYYY')}</text>
+      <view className='DayContainer'>
+        <view
+          className='BackArrowContainer'
+          bindtap={() => nav('/')}
+        >
+          <image
+            className='BackArrow'
+            src={LeftArrow}
+          />
+        </view>
         <scroll-view className='DayContainer'>
           {habits.map((h, habitIndex) => {
             const value = getDayHabitValue(dateIndex, habitIndex);
